14489042885abbbe83211f52613a84624be9f8dd
Magno Kelly <mkfelipe.rj@gmail.com>
2023-02-25 08:12:54 -0300

    anchors ref

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
     const [darkMode, setDarkMode] = useState(false)
     return (
         <>
-            <div className={darkMode ? 'dark' : '' }>
+            <div className={darkMode ? 'dark' : ''}>
                 <Head>
                     <title>Magno Kelly Portfolio</title>
                     <meta name="description" content="Porfolio do Magno Kelly"/>
@@ -33,7 +33,7 @@ export default function Home() {
                 <main className={'bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40'}>
                     <section className={'min-h-screen'}>
                         <nav className={'py-10 mb-12 flex justify-between dark:text-white'}>
-                            <h1 className={'text-xl font-burtons'}>developbyMK</h1>
+                            <h1 className={'text-xl font-burtons'}>developedbyMK</h1>
                             <ul className={'flex items-center'}>
                                 <li>
                                     <BsFillMoonStarsFill className={'cursor-pointer text-2xl'}
@@ -50,17 +50,38 @@ export default function Home() {
                         <div className={'text-center p-10 py-10'}>
                             <h2 className={'text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl'}>Magno
                                 Kelly</h2>
-                            <h3 className={'text-2xl py-2 dark:text-white md:text-3xl'}>Software Engineer | Front-End React
+                            <h3 className={'text-2xl py-2 dark:text-white md:text-3xl'}>Software Engineer | Front-End
+                                React
                                 Heated | Back-End PhP(Laravel) Enthusiastic </h3>
                             <p className={'text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl'}>
-                                Freelancer technology passionate based in Rio de Janeiro - Brazil, committed to working with
+                                Freelancer technology passionate based in Rio de Janeiro - Brazil, committed to working
+                                with
                                 you every step of the way to ensure your success. Join me down below to see my work.
                             </p>
 
-                            <div className={'text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400'}>
-                                <AiFillTwitterCircle/>
-                                <AiFillGithub/>
-                                <AiFillLinkedin/>
+                            <div
+                                className={'text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400'}>
+                                <a
+                                    href="https://twitter.com/magnokf"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <AiFillTwitterCircle/>
+                                </a>
+                                <a
+                                    href="https://github.com/magnokf"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <AiFillGithub/>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/magno-kelly-felipe-76778a233/"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <AiFillLinkedin/>
+                                </a>
                             </div>
                             <div
                                 className={'relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden mt-20 md:h-96 md:w-96'}>
@@ -74,14 +95,17 @@ export default function Home() {
                             <h3 className={'text-3xl py-1 dark:text-white'}>Services I offer</h3>
                             <p className={'text-md py-2 leading-8 text-gray-800 dark:text-gray-200'}>
                                 Since I started my career as a freelancer, I have been working with a
-                                <span className={'text-teal-500'}> wide range of technologies and tools</span>. I have worked
+                                <span className={'text-teal-500'}> wide range of technologies and tools</span>. I have
+                                worked
                                 with
-                                <span className={'text-teal-500'}> many types of projects</span>, from small to large scale.
+                                <span className={'text-teal-500'}> many types of projects</span>, from small to large
+                                scale.
                             </p>
                             <p className={'text-md py-2 leading-8 text-gray-800 dark:text-gray-200'}>
                                 {/* eslint-disable-next-line react/no-unescaped-entities */}
                                 Whether it's selecting the right technologies, identifying potential roadblocks, or
-                                brainstorming <span className={'text-teal-500'}> creative solutions</span>, I approach every
+                                brainstorming <span className={'text-teal-500'}> creative solutions</span>, I approach
+                                every
                                 project with a consultative mindset. My goal is to not only deliver <span
                                 className={'text-teal-500'}>high-quality software</span>, but also to build long-term
                                 partnerships with my clients by providing them with <span className={'text-teal-500'}>exceptional value and outstanding service</span>.
@@ -93,7 +117,8 @@ export default function Home() {
                                 <h3 className={'text-lg font-medium pt-8 pb-2'}>Beautiful Designs</h3>
                                 <p className={'py-2'}>
                                     Creating a beautiful design is one of the most important steps in the development
-                                    process. I work closely with my clients to create a design that is both beautiful and
+                                    process. I work closely with my clients to create a design that is both beautiful
+                                    and
                                     functional.
                                 </p>
                                 <h4 className={'py-4 text-teal-600'}>Design tools I use</h4>
@@ -105,7 +130,10 @@ export default function Home() {
                                 <Image src={code} alt={'design'} width={100} height={100}/>
                                 <h3 className={'text-lg font-medium pt-8 pb-2'}>Strong technical skills</h3>
                                 <p className={'py-2'}>
-                                    Reliable understanding of programming concepts, algorithms, data structures, and software development principles. They should be skilled in multiple programming languages and frameworks, and be able to write high-quality, efficient, and maintainable code.
+                                    Reliable understanding of programming concepts, algorithms, data structures, and
+                                    software development principles. They should be skilled in multiple programming
+                                    languages and frameworks, and be able to write high-quality, efficient, and
+                                    maintainable code.
                                 </p>
                                 <h4 className={'py-4 text-teal-600'}>About me</h4>
                                 <p className={'text-gray-800 py-1'}>Depth of knowledge</p>
@@ -116,7 +144,11 @@ export default function Home() {
                                 <Image src={consulting} alt={'design'} width={100} height={100}/>
                                 <h3 className={'text-lg font-medium pt-8 pb-2'}>Good communication skills</h3>
                                 <p className={'py-2'}>
-                                    Team effort, and a reliable software engineer should be able to communicate effectively with other team members, including other developers, project managers, and stakeholders. They should be able to clearly explain technical concepts and requirements, and be willing to collaborate and work together to find the best solutions to problems.
+                                    Team effort, and a reliable software engineer should be able to communicate
+                                    effectively with other team members, including other developers, project managers,
+                                    and stakeholders. They should be able to clearly explain technical concepts and
+                                    requirements, and be willing to collaborate and work together to find the best
+                                    solutions to problems.
                                 </p>
                                 <h4 className={'py-4 text-teal-600'}>About me</h4>
                                 <p className={'text-gray-800 py-1'}>Clarity</p>
@@ -147,7 +179,7 @@ export default function Home() {
                             <div className="basis-1/3 flex-1">
                                 <Image
                                     className="rounded-lg object-cover"
-                                    
+
                                     src={web1}
                                     alt={'web1'}
                                 />
@@ -155,7 +187,7 @@ export default function Home() {
                             <div className="basis-1/3 flex-1">
                                 <Image
                                     className="rounded-lg object-cover"
-                                    
+
                                     src={web2}
                                     alt={'web2'}
                                 />
@@ -163,7 +195,7 @@ export default function Home() {
                             <div className="basis-1/3 flex-1">
                                 <Image
                                     className="rounded-lg object-cover"
-                                    
+
                                     src={web5}
                                     alt={'web3'}
                                 />
@@ -171,7 +203,7 @@ export default function Home() {
                             <div className="basis-1/3 flex-1">
                                 <Image
                                     className="rounded-lg object-cover"
-                                    
+
                                     src={web5}
                                     alt={'web4'}
                                 />
